fc1725b338425920820f52d08e3587fe1c0d5081
diazabdi257 <diazabdi2003@gmail.com>
2025-10-29 15:07:44 +0700

    add product section

--- Script/script.js
+++ Script/script.js
@@ -12,16 +12,22 @@ document.querySelector('#hamburger-menu').onclick = () => {
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
 
-document.querySelector('#search-button').onclick = () =>{
+document.querySelector('#search-button').onclick = (e) =>{
     searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
 }
 
 // Klik diluar sidebar untuk menghilangkan nav
 
-const hamburger = document.querySelector("#hamburger-menu");
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+       searchForm.classList.remove("active");
+     }
 })
