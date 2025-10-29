165d19cb5142028dc23c30a8fd98e7ca934bf53f
diazabdi257 <diazabdi2003@gmail.com>
2025-10-30 02:03:04 +0700

    shopping cart and modal box

--- Script/script.js
+++ Script/script.js
@@ -17,11 +17,17 @@ document.querySelector('#search-button').onclick = (e) =>{
     searchBox.focus();
     e.preventDefault();
 }
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
 
+document.querySelector("#shopping-cart-button").onclick = () => {
+  shoppingCart.classList.toggle("active");
+};
 // Klik diluar sidebar untuk menghilangkan nav
 
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener('click', function(e){
     if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
@@ -29,5 +35,8 @@ document.addEventListener('click', function(e){
     }
     if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
        searchForm.classList.remove("active");
+     }
+     if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+       shoppingCart.classList.remove("active");
      }
 })
