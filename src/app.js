058992d71620c8d6906190892d4a4c59f37870a2
diazabdi257 <diazabdi2003@gmail.com>
2025-11-22 02:07:39 +0700

    payment gateway pt1

--- src/app.js
+++ src/app.js
@@ -69,8 +69,74 @@ document.addEventListener("alpine:init", () => {
         })
       }
     },
+    remove(id){
+      // ambil item yang mau diremove berdasarkan idnya
+      const cartItem = this.items.find((item) => item.id === id);
+
+      // jika item lebih dari 1
+      if(cartItem.quantity > 1){
+        // telusuri 1 1
+        this.items = this.items.map((item) => {
+          // jika bukan barang yang diklik
+          if(item.id !==id){
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        })
+      } else if (cartItem.quantity === 1) {
+        // jika barangnya sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    }
   });
 });
+
+// form validation
+const checkoutButton = document.querySelector('.checkout-button');
+checkoutButton.disabled = true;
+
+const form = document.querySelector('#checkoutForm');
+
+form.addEventListener('keyup', function () {
+  for(let i = 0; i < form.elements.length; i++){
+    if(form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove('disabled');
+      checkoutButton.classList.add("disabled");
+    } else{
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove('disabled')
+});
+// kirim data ketika tombol checkout diklik
+checkoutButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open('http://wa.me/6285219752428?text=' + encodeURIComponent(message));
+})
+
+// format pesan Whatsapp
+const formatMessage = (obj) => {
+  return `Data Customer
+    Nama: ${obj.name}
+    Email: ${obj.email}
+    No HP: ${obj.phone}
+Data Pesanan
+  ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)})\n`)}
+TOTAL: ${rupiah(obj.total)}
+Terima Kasih.`;
+}
 
 // konversi ke Rupiah
 const rupiah = (number) => {
